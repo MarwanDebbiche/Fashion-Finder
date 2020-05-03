bf815846b1c133e714694231bb16a8bc687c9977
Marwan Debbiche (Macbook Pro) <marwan.debbiche@capgemini.com>
2020-05-03 23:09:50 +0200

    WIP: Asos Scraper

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Fashion-Finder
+
 Fashion Finder Repository
+
+## Scraping
+
+```
+cd src/scraping
+```
+
+**Asos:**
+
+```
+scrapy crawl asos -o scraping/data/asos/asos.json
+```
--- src/scraping/scraping/spiders/asos.py
+++ src/scraping/scraping/spiders/asos.py
@@ -1,24 +1,59 @@
-import scrapy
 import os
+import scrapy
 
 
 class AsosSpider(scrapy.Spider):
     name = "asos"
+
+    categories = [
+        3602,  # men shirts
+        4209,  # men shoes
+        5668,  # men sweaters
+        4208,  # men jeans
+        3606  # men vest &coat
+    ]
+
     start_urls = [
-        'https://www.asos.com/fr/homme/chaussures-bottes-baskets/cat/?cid=4209&cr=4&page=1'
+        f'https://www.asos.com/fr/cat/?cid={cat}&page=1'
+        for cat in categories
     ]
 
     def parse(self, response):
-        articles = response.xpath(
-            '//div[@data-auto-id="productList"]//article')
-        for article in articles:
-            product_url = article.xpath('a/@href').extract_first()
-            product_desc = article.xpath(
-                'a//div[@data-auto-id="productTileDescription"]//p/text()').extract_first()
-            data = {}
-            data['url'] = product_url
-            data['description'] = product_desc
-            yield data
+        article_page_links = response.xpath(
+            '//div[@data-auto-id="productList"]//article/a')
+        yield from response.follow_all(article_page_links, self.parse_article)
+
+        next_page = response.xpath(
+            '//a[@data-auto-id="loadMoreProducts"]/@href').extract_first()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_article(self, response):
+
+        images_url = [
+            image_url.split("?")[0]
+            for image_url
+            in response.xpath('//*[@class="gallery-aside-wrapper"]//li//@src').getall()
+        ]
+
+        infos = [
+            info.strip() for info in
+            (
+                response.xpath('//*[@class="about-me"]//text()').getall() +
+                response.xpath(
+                    '//div[@class="product-description"]//text()').getall()
+            )
+            if len(info.strip()) > 1
+        ]
+
+        yield {
+            'product_url': response.request.url,
+            'name': response.xpath('//*[@class="layout-aside"]//h1/text()').get(),
+            'brand_url': response.xpath('//*[@class="brand-description"]/p//@href').get(),
+            'images': images_url,
+            'infos': infos
+        }
 
     def on_error(self, error):
         print(error)
